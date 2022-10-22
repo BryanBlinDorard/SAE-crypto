4a77ce0fbaf490a0d6aa32094d0d356cd67aa2ce
Constantin LOSZACH <constantinlozach@gmail.com>
2022-10-22 10:30:13 +0200

    Modification des noms de variables et corrections de petites fautes.

--- src/affine.py
+++ src/affine.py
@@ -45,23 +45,22 @@ def calcul_distance(textes_possibles:dict):
     Returns:
         dict: Un dictionnaire avec toutes les possibilités de décodage d'un texte et leur distance.
     """
-    tableau_de_freq_global=dict()
+    tableau_de_freq=dict()
     distance = dict()
 
     #calcul de la fréquence de chaque lettre pour chaque possibilité
     for texte in textes_possibles:
         texte = fonctions_utiles.supprimer_caracteres_non_lettres(texte)
-        freq_lettre_du_texte = fonctions_utiles.calculer_frequence_lettre(texte)
-        tableau_de_freq_global[texte] = freq_lettre_du_texte
-    
+        freq_lettres_du_texte = fonctions_utiles.calculer_frequence_lettre(texte)
+        tableau_de_freq[texte] = freq_lettres_du_texte
+
     #calcul de la distance entre la fréquence de chaque lettre et la fréquence de la langue FR
-    for texte in tableau_de_freq_global:
+    for texte in tableau_de_freq:
         calcul=0
-        for lettre in tableau_de_freq_global[texte]:
-            calcul+=(CONSTANTES.FR_FREQ[lettre]-tableau_de_freq_global[texte][lettre])**2
+        for lettre in tableau_de_freq[texte]:
+            calcul+=(CONSTANTES.FR_FREQ[lettre]-tableau_de_freq[texte][lettre])**2
         calcul=math.sqrt(calcul)  
         distance[texte]=calcul
-
     return distance
 
 
@@ -69,18 +68,18 @@ def distance_la_plus_petite(distance:dict)->str:
     """Permet de trouver le texte avec la distance la plus petite.
 
     Args:
-        distance (dict): Un dictionnaire avec toutes les possibilités de décodage d'un texte et leur distance.
+        distance (dict): Un dictionnaire avec toutes les possibilités de décodage d'un texte et leurs distances.
 
     Returns:
         str: Le texte avec la distance la plus petite.
     """
 
-    mini = None
+    distance_mini = None
     for texte in distance:
-        if mini == None:
-            mini = distance[texte]
-        if distance[texte] < mini:
-            mini = distance[texte]
+        if distance_mini == None:
+            distance_mini = distance[texte]
+        if distance[texte] < distance_mini:
+            distance_mini = distance[texte]
             texte_mini = texte
     return texte_mini
 
@@ -89,20 +88,20 @@ def test_toutes_les_possibilites(code:str):
     """Permet de générer un dictionnaire avec toutes les possibilités de décodage d'un texte.
 
     Args:
-        code (str): Un code crypté avec le chiffrement affine
+        code (str): Un code crypté avec le chiffrement affine.
 
     Returns:
         dict: Un dictionnaire avec toutes les possibilités de décodage d'un texte.
     """    
-    dico = {}
+    possibilites = {}
     for premier_avec_26 in CONSTANTES.PREMIERS_AVEC_26:
         for i in range(26):
             phrase_potentiel_decodage = decode_affine(code,(premier_avec_26,i))
-            dico[phrase_potentiel_decodage] = (premier_avec_26,i)
-    return dico
+            possibilites[phrase_potentiel_decodage] = (premier_avec_26,i)
+    return possibilites
 
 def casser_affine_auto(texte_a_decoder:str)->str:
-    """Permet de casser un message coder avec chiffrement affine.
+    """Permet de casser un message codé avec chiffrement affine.
 
     Args:
         texte (str): Un texte à décoder.
@@ -112,9 +111,9 @@ def casser_affine_auto(texte_a_decoder:str)->str:
     """    
     texte_a_decoder = texte_a_decoder.upper()
 
-    dico_avec_les_possibilites = test_toutes_les_possibilites(texte_a_decoder)
-    dico_de_distance = calcul_distance(dico_avec_les_possibilites)
-    texte = distance_la_plus_petite(dico_de_distance)
+    possibilites = test_toutes_les_possibilites(texte_a_decoder)
+    distance = calcul_distance(possibilites)
+    texte = distance_la_plus_petite(distance)
     
-    texte_correctement_ecrit = fonctions_utiles.ajouter_caracteres_non_lettres(texte_a_decoder,texte)
-    return texte_correctement_ecrit
+    texte_final = fonctions_utiles.ajouter_caracteres_non_lettres(texte_a_decoder,texte)
+    return texte_final
